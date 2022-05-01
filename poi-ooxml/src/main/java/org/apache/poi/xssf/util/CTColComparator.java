8e7dddfc6311e676050b79d3561fc807986a37f6
rodrigotabsan <rodrigotablado@hotmail.com>
2022-05-01 04:45:17 +0200

    Update CTColComparator.java
    improving the readability of the class CTColComparator

--- poi-ooxml/src/main/java/org/apache/poi/xssf/util/CTColComparator.java
+++ poi-ooxml/src/main/java/org/apache/poi/xssf/util/CTColComparator.java
@@ -37,9 +37,17 @@ public class CTColComparator {
     public static final Comparator<CTCol> BY_MIN_MAX = new Comparator<CTCol>() {
         @Override
         public int compare(CTCol col1, CTCol col2) {
+            int result = 0;
             long col11min = col1.getMin();
             long col2min = col2.getMin();
-            return col11min < col2min ? -1 : col11min > col2min ? 1 : BY_MAX.compare(col1, col2);
+            if(col11min < col2min) {
+               result = -1;
+            } else if(col11min > col2min) {
+               result = 1;  
+            } else {
+               result = BY_MAX.compare(col1, col2);
+            }
+            return result;
         }
     };
 
